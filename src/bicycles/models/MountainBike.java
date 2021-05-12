ff197453033ce12846df8b127667bcf9c6305af1
LuyoloLuja <luyolom19@gmail.com>
2021-05-12 11:29:34 +0200

    removed main method on mountain bike

--- src/bicycles/models/MountainBike.java
+++ src/bicycles/models/MountainBike.java
@@ -3,13 +3,12 @@ package bicycles.models;
 import bicycles.BicycleBase;
 
 public class MountainBike extends BicycleBase {
-
-    public static  void main(String[] args){
-        MountainBike mountainBike = new MountainBike();
-        mountainBike.accelerate();
-        System.out.println(mountainBike.currentSpeed());
-    }
     public void accelerate(){ changeSpeed(5); }
     public void brake(){ changeSpeed(-3); }
 
+//    public static  void main(String[] args){
+//        MountainBike mountainBike = new MountainBike();
+//        mountainBike.accelerate();
+//        System.out.println(mountainBike.currentSpeed());
+//    }
 }
