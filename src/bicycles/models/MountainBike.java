39b4d953d57f1e2b88f8d5c01bab9bbbcfb5adb2
LuyoloLuja <luyolom19@gmail.com>
2021-05-17 15:25:34 +0200

    added fun ride

--- src/bicycles/models/MountainBike.java
+++ src/bicycles/models/MountainBike.java
@@ -7,7 +7,6 @@ public class MountainBike extends BicycleBase {
     public void accelerate(){ changeSpeed(5); }
     public void brake(){ changeSpeed(-3); }
 
-    @Override
     public BicycleType getBicycleType() {
         return BicycleType.MountainBike;
     }
